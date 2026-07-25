c8b598a2c4af489fec660ba5acd93a48382e94d3
secops-dev-22 <9@felix-au.me>
2026-07-25 15:18:01 +0530

    Implement features in tictactoe.cpp - Version 5

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -3,3 +3,5 @@
 
 void drawBoard(const std::vector<char>& board) {
     std::cout << " " << board[0] << " | " << board[1] << " | " << board[2] << "\n";
+    std::cout << "---|---|---\n";
+    std::cout << " " << board[3] << " | " << board[4] << " | " << board[5] << "\n";
